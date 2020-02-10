9f11cb1c2c5cd2bd763ef8fdf21e74b3864e0ac7
Luiz Adamchuk <adis_jr@hotmail.com>
2020-02-10 09:42:19 -0300

    Insere mensagem de confirmação na pg  do ato feito

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -8,6 +8,10 @@ class NegociacaoController {
 
         this._negociacoesView = new NegociacoesView(document.querySelector("#negociacoesView"));
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView(document.querySelector("#mensagemView"));
+        this._mensagemView.update(this._mensagem);
     }
 
 
@@ -17,6 +21,10 @@ class NegociacaoController {
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = "Negociação adicionada com sucesso."
+        this._mensagemView.update(this._mensagem);
+
         this._limpaFormulario();
 
     }
